194e1b8dce0d915ecdec91fc49060bca3d7e9c94
emon5419 <alauddinemon3@gmail.com>
2023-05-13 21:27:46 +0600

    SetUpFirebaseApp-50%Complate

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,38 @@
+import firebase from 'firebase/compat/app';
+import { initializeApp } from "firebase/app";
+import phoneAuth from '../phoneAuth';
+import { useEffect, useState } from 'react';
+import { onAuthStateChange } from 'firebase/auth';
+
+
 export default function Home() {
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyA1LnYi-Zbp-uji54U6OXxzvgH2cBK9mqk",
+    authDomain: "bazar-bd.firebaseapp.com",
+    projectId: "bazar-bd",
+    storageBucket: "bazar-bd.appspot.com",
+    messagingSenderId: "102078290891",
+    appId: "1:102078290891:web:c0b2e81487a3b7d9e8b44e"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unRegistered = onAuthStateChange(firebase.auth(), (currentUser) => {
+      console.log(currentUser);
+      setUser(currentUser);
+    })
+  })
+
+
+
+
   return (
     <div className="w-screen h-screen">
-      <h1>hello world</h1>
+      <phoneAuth auth={firebase.auth()}></phoneAuth>
     </div>
   )
 }
---
+++ src/phoneAuth.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import firebase from 'firebase/compat/app';
+import * as firebaseui from 'firebaseui';
+import 'firebaseui/dist/firebaseui.css';
+
+const phoneAuth = () => {
+   return (
+      <div>
+         
+      </div>
+   );
+};
+
+export default phoneAuth;
